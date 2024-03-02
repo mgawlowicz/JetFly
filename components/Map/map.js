362e03ae81a676776d90b10d4ffafd18f83f84d7
mgawlowicz <251141@edu.p.lodz.pl>
2024-03-02 23:09:54 +0100

    text in office map section was changed

--- components/Map/map.js
+++ components/Map/map.js
@@ -6,7 +6,7 @@ export default function Map(){
         <div className="flex flex-col lg:flex-row-reverse gap-12 px-4 lg:px-16">
             <div className="flex flex-col gap-2">
                 <h3 className="uppercase font-bold text-2xl lg:text-4xl">Around the world</h3>
-                <p className="lg:text-xl text-gray-400">At JetFly, we understand that the world is your playground. That's why we've strategically placed our offices across the globe, ensuring that no matter where you are, we're nearby to cater to your every travel need.</p>
+                <p className="lg:text-xl text-gray-400">At JetFly, we understand that the world is your playground. That is why we have strategically placed our offices across the globe, ensuring that no matter where you are, we are nearby to cater to your every travel need.</p>
             </div>
             <div>
                 <Image src={'/map.webp'} width={1920} height={1080} loading="lazy" alt="offices map"/>
