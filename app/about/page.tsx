2fd432b4ca3cb8b772d2939433c9057f560219a0
mgawlowicz <251141@edu.p.lodz.pl>
2024-03-04 15:35:05 +0100

    fixed mobile text fonts on about us subpage

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -18,22 +18,22 @@ export default function Home() {
             </div>
             <div className="w-full px-4 lg:px-16 flex flex-col lg:flex-row gap-8">
                 <div className="w-full lg:w-1/3 flex flex-col gap-2">
-                    <h4 className="text-2xl lg:text-4xl font-bold">Comfort</h4>
+                    <h4 className="text-xl lg:text-3xl font-bold">Comfort</h4>
                     <p className="text-gray-400 lg:text-xl">We ensure comfort by offering the flexibility to position the aircraft to any airport within the continent, providing flexibility and luxury in travel.</p>
                 </div>
                 <div className="w-full lg:w-1/3 flex flex-col gap-2">
-                    <h4 className="text-2xl lg:text-4xl font-bold">Insurance</h4>
+                    <h4 className="text-xl lg:text-3xl font-bold">Insurance</h4>
                     <p className="text-gray-400 lg:text-xl">We provide insurance for all flights, allowing our clients to travel worry-free and with complete peace of mind.</p>
                 </div>
                 <div className="w-full lg:w-1/3 flex flex-col gap-2">
-                    <h4 className="text-2xl lg:text-4xl font-bold">Services 24/7</h4>
+                    <h4 className="text-xl lg:text-3xl font-bold">Services 24/7</h4>
                     <p className="text-gray-400 lg:text-xl">Our support team is available 24/7 to provide you with assistance and professional service at any moment of your journey.</p>
                 </div>
             </div>
             <div className="flex flex-col px-4 lg:px-16 gap-12">
-                <div className="flex flex-col lg:flex-row gap-12">
+                <div className="flex flex-col lg:flex-row gap-4 lg:gap-12">
                     <div className="w-full flex flex-col gap-2">
-                        <h2 className="uppercase font-bold text-4xl">Who we are</h2>
+                        <h2 className="uppercase font-bold text-2xl lg:text-4xl">Who we are</h2>
                         <p className="text-gray-400 lg:text-xl">We are a leading luxury charter company committed to elevating the standards of air travel. With a focus on excellence and unparalleled service, we provide discerning travelers with exceptional experiences in the skies.</p>
                     </div>
                     <div className="w-full">
@@ -59,31 +59,31 @@ export default function Home() {
             </div>
             <div className="flex flex-col lg:flex-row gap-12 px-4 lg:px-16">
                 <div className="flex flex-col gap-2 w-full">
-                    <h2 className="uppercase font-bold text-4xl">Visionary Founder</h2>
+                    <h2 className="uppercase font-bold text-2xl lg:text-4xl">Visionary Founder</h2>
                     <p className="text-gray-400 lg:text-xl">Benjamin Hughes, refused to settle for the status quo. His dream of luxurious travel experiences where comfort and customer service reign supreme led to the creation of JetFly. It&apos;s more than just a company; it&apos;s a vision to revolutionize the standards of business aviation. Thanks to his passion and determination, JetFly doesn&apos;t just offer flights; it crafts unforgettable experiences that transcend ordinary travel.</p>
                 </div>
                 <Image src={'/founder-picture.jpeg'} width={600} height={400} alt="founder-picture" className="w-full"/>
             </div>
-            <div className="flex flex-col gap-12 px-4 lg:px-16">
-                <h2 className="uppercase font-bold text-4xl">JetFly in numbers</h2>
-                <div className="flex flex-col gap-12">
-                    <div className="flex flex-col lg:flex-row gap-12">
+            <div className="flex flex-col gap-4 lg:gap-12 px-4 lg:px-16">
+                <h2 className="uppercase font-bold text-2xl lg:text-4xl">JetFly in numbers</h2>
+                <div className="flex flex-col gap-2 lg:gap-12">
+                    <div className="flex flex-col lg:flex-row gap-2 lg:gap-12">
                         <div className="w-full flex justify-between items-end py-4 border-b border-solid border-gray-400">
-                            <h3 className="text-6xl">520+</h3>
+                            <h3 className="text-4xl lg:text-6xl">520+</h3>
                             <p className="lg:text-xl">Clients</p>
                         </div>
                         <div className="w-full flex justify-between items-end py-4 border-b border-solid border-gray-400">
-                            <h3 className="text-6xl">10K+</h3>
+                            <h3 className="text-4xl lg:text-6xl">10K+</h3>
                             <p className="lg:text-xl">Hours in sky</p>
                         </div>
                     </div>
-                    <div className="flex flex-col lg:flex-row gap-12">
+                    <div className="flex flex-col lg:flex-row gap-2 lg:gap-12">
                         <div className="w-full flex justify-between items-end py-4 border-b border-solid border-gray-400">
-                            <h3 className="text-6xl">9M</h3>
+                            <h3 className="text-4xl lg:text-6xl">9M</h3>
                             <p className="lg:text-xl">Kilometers flown</p>
                         </div>
                         <div className="w-full flex justify-between items-end py-4 border-b border-solid border-gray-400">
-                            <h3 className="text-6xl">93%</h3>
+                            <h3 className="text-4xl lg:text-6xl">93%</h3>
                             <p className="lg:text-xl">Repeat Clients</p>
                         </div>
                     </div>
