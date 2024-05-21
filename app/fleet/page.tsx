edeecdd6fd6e17cbf913e528ee85fbdf9dc5621d
mgawlowicz <251141@edu.p.lodz.pl>
2024-05-21 18:31:51 +0200

    changed structure of files in 'our fleet' subpae

--- app/fleet/page.tsx
+++ app/fleet/page.tsx
@@ -1,35 +1,8 @@
-"use client"
-import { useEffect, useState } from 'react';
 import Image from 'next/image';
-import Link from 'next/link';
 import Form from '@/components/Form/form';
-import { motion } from 'framer-motion'
-
-interface Plane {
-  brand: string;
-  model: string;
-  details: {
-    price: string;
-  };
-  image: {
-    url: string,
-    alt: string,
-  }
-  slug: string;
-}
+import Jets from './components/jets';
 
 export default function Home(){
-  const [planes, setPlanes] = useState<Plane[]>([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const res = await fetch('api/jets');
-      const data = await res.json();
-      setPlanes(Object.values(data.data));
-    };
-
-    fetchData();
-  }, []);
 
   return (
     <main className='flex flex-col gap-24 lg:gap-36'>
@@ -49,29 +22,7 @@ export default function Home(){
             <option>Sort by</option>
           </select>
         </div>
-        <div className='grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3'>
-          {planes.map((plane, index) => (
-            <Link href={`/fleet/${plane.slug}`} key={index} className='relative'>
-              <div className='w-full h-full'>
-                <Image src={`/Jets/${plane.slug}/${plane.image.url}`} width={900} height={600} alt={plane.image.alt} className='brightness-75 object-cover'/>
-              </div>
-              <div className='w-full h-full absolute top-0 left-0 p-8 xl:p-12 flex flex-col justify-between'>
-                <h3 className='lg:text-xl font-semibold'>{plane.brand}</h3>
-                <h2 className='text-4xl xl:text-5xl font-bold'>{plane.model}</h2>
-                <div className='flex justify-between w-full items-end'>
-                  <div>
-                    <h5 className='font-semibold'>Starting at</h5>
-                    <h5 className='lg:text-xl font-semibold'>${plane.details.price}/h</h5>
-                  </div>
-                  <div className="flex gap-2 justify-end">
-                    <p className='font-semibold'>View</p>
-                    <Image src={"/arrow_down.png"} width={24} height={24} alt='arrow-icon' className='rotate-270'/>
-                  </div>
-                </div>
-              </div>
-            </Link>
-          ))}
-        </div>
+        <Jets />
       </div>
       <Form />
     </main>
