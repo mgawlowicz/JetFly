e62979483aad03f039747a29b5d08439165ad8bf
mgawlowicz <251141@edu.p.lodz.pl>
2024-05-21 18:15:11 +0200

    fixed displaying a fleet on 'our fleet' subpage

--- app/fleet/page.tsx
+++ app/fleet/page.tsx
@@ -18,7 +18,7 @@ interface Plane {
   slug: string;
 }
 
-const YourComponent = () => {
+export default function Home(){
   const [planes, setPlanes] = useState<Plane[]>([]);
 
   useEffect(() => {
@@ -30,6 +30,7 @@ const YourComponent = () => {
 
     fetchData();
   }, []);
+
   return (
     <main className='flex flex-col gap-24 lg:gap-36'>
       <div className='flex flex-col pt-36 gap-8'>
@@ -48,35 +49,31 @@ const YourComponent = () => {
             <option>Sort by</option>
           </select>
         </div>
-        <div className='flex flex-wrap flex-col lg:flex-row gap-4 lg:gap-0'>
+        <div className='grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3'>
           {planes.map((plane, index) => (
-            <div key={index} className='relative overflow-hidden w-full lg:w-1/3 lg:px-2 lg:py-2'>
-              <Link href={`/fleet/${plane.slug}`}>
-                <div className="w-full h-full relative">
-                  <Image src={`/Jets/${plane.slug}/${plane.image.url}`} width={900} height={600} alt={plane.image.alt} className='brightness-75' />
-                </div>
-                <div className='absolute top-0 p-8 lg:p-12 flex flex-col w-full h-full justify-between'>
-                  <h3 className='lg:text-xl font-semibold'>{plane.brand}</h3>
-                  <h2 className='text-4xl lg:text-5xl font-bold'>{plane.model}</h2>
-                  <div className='flex justify-between w-full items-end'>
-                    <div>
-                      <h5 className='font-semibold'>Starting at</h5>
-                      <h5 className='lg:text-xl font-semibold'>${plane.details.price}/h</h5>
-                    </div>
-                    <div className='flex gap-2 justify-end'>
-                      <p className='font-semibold'>View</p>
-                      <Image src={"/arrow_down.png"} width={24} height={24} alt='arrow-icon' className='rotate-270'/>
-                    </div>
+            <Link href={`/fleet/${plane.slug}`} key={index} className='relative'>
+              <div className='w-full h-full'>
+                <Image src={`/Jets/${plane.slug}/${plane.image.url}`} width={900} height={600} alt={plane.image.alt} className='brightness-75 object-cover'/>
+              </div>
+              <div className='w-full h-full absolute top-0 left-0 p-8 xl:p-12 flex flex-col justify-between'>
+                <h3 className='lg:text-xl font-semibold'>{plane.brand}</h3>
+                <h2 className='text-4xl xl:text-5xl font-bold'>{plane.model}</h2>
+                <div className='flex justify-between w-full items-end'>
+                  <div>
+                    <h5 className='font-semibold'>Starting at</h5>
+                    <h5 className='lg:text-xl font-semibold'>${plane.details.price}/h</h5>
+                  </div>
+                  <div className="flex gap-2 justify-end">
+                    <p className='font-semibold'>View</p>
+                    <Image src={"/arrow_down.png"} width={24} height={24} alt='arrow-icon' className='rotate-270'/>
                   </div>
                 </div>
-              </Link>
-            </div>
+              </div>
+            </Link>
           ))}
         </div>
       </div>
       <Form />
     </main>
   );
-};
-
-export default YourComponent;
+}
